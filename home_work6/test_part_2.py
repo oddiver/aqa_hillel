1d822c9ce816cf94b2c17f3a7d8308876f71371a
Sergey <63729662+oddiver@users.noreply.github.com>
2023-01-21 15:14:39 +0100

    Update test_part_2.py
    updated

--- home_work6/test_part_2.py
+++ home_work6/test_part_2.py
@@ -4,17 +4,20 @@ import requests
 
 def test_six():
     response = requests.get("https://aqa.science/")
+    assert response.status_code == 200
     print(response.status_code)
     print(response.text)
 
 
 def test_seven():
     response = requests.get("https://aqa.science/users")
+    assert response.status_code == 401 or 403
     print(response.status_code)
     print(response.text)
 
 
 def test_eight():
     response = requests.get("https://aqa.science/users", auth=("admin","admin123"))
+    assert response.status_code == 200
     print(response.status_code)
     print(response.text)
